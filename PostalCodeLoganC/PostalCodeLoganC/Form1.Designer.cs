7b0aff7923e8857561dbb0a3fb2ebc7a902abdbe
Logan Cantin <logan.cantin@gmail.com>
2019-05-28 20:19:28 -0400

    canada post

---
+++ PostalCodeLoganC/PostalCodeLoganC/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace PostalCodeLoganC
+{
+    partial class frmPostal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPostalcode = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPostal = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.txtProvince = new System.Windows.Forms.TextBox();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(77, 70);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(36, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "name:";
+            // 
+            // lblPostalcode
+            // 
+            this.lblPostalcode.AutoSize = true;
+            this.lblPostalcode.Location = new System.Drawing.Point(80, 109);
+            this.lblPostalcode.Name = "lblPostalcode";
+            this.lblPostalcode.Size = new System.Drawing.Size(62, 13);
+            this.lblPostalcode.TabIndex = 1;
+            this.lblPostalcode.Text = "postal code";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(153, 67);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtPostal
+            // 
+            this.txtPostal.Location = new System.Drawing.Point(153, 106);
+            this.txtPostal.Name = "txtPostal";
+            this.txtPostal.Size = new System.Drawing.Size(100, 20);
+            this.txtPostal.TabIndex = 3;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(153, 194);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 7;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(153, 155);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "city:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "address:";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(153, 246);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(100, 20);
+            this.txtCountry.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(77, 249);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Country";
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(124, 317);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 11;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.BtnDisplay_Click);
+            // 
+            // txtProvince
+            // 
+            this.txtProvince.Location = new System.Drawing.Point(153, 220);
+            this.txtProvince.Name = "txtProvince";
+            this.txtProvince.Size = new System.Drawing.Size(100, 20);
+            this.txtProvince.TabIndex = 13;
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Location = new System.Drawing.Point(77, 223);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(48, 13);
+            this.lblProvince.TabIndex = 12;
+            this.lblProvince.Text = "province";
+            // 
+            // frmPostal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 414);
+            this.Controls.Add(this.txtProvince);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPostal);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblPostalcode);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmPostal";
+            this.Text = "Postal Code by Logan Cantin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPostalcode;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPostal;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.TextBox txtProvince;
+        private System.Windows.Forms.Label lblProvince;
+    }
+}
+
---
+++ PostalCodeLoganC/PostalCodeLoganC/Form1.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Created by: Logan Cantin 
+ * Created on: May 28, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #42 - Canada Post Struct
+ * This program collects user information and holds it in a struct
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PostalCodeLoganC
+{
+    public partial class frmPostal : Form
+    {
+        public frmPostal()
+        {
+            InitializeComponent();
+        }
+
+
+        public struct PostalAddress
+        {
+            public string name, postalcode, address, city, province, country;
+
+            public PostalAddress(string n, string p, string a, string c, string pr, string coun)
+            {
+                name = n;
+                postalcode = p;
+                address = a;
+                city = c;
+                province = pr;
+                country = coun;
+            }
+        }
+    
+        private void BtnDisplay_Click(object sender, EventArgs e)
+        {
+            // setting variables
+            string name = txtName.Text;
+            string postalcode = txtPostal.Text;
+            string address = txtAddress.Text;
+            string city = txtCity.Text;
+            string province = txtProvince.Text;
+            string country = txtCountry.Text;
+
+            // Creating object
+            PostalAddress dest = new PostalAddress(name, postalcode, address, city, province, country);
+
+            // Show the information in a formatted way
+            MessageBox.Show(string.Format("{0} who lives at {1}, {2}. They live in {3}, {4}, {5}.", dest.name, dest.postalcode, dest.address,
+                dest.city, dest.province, dest.country));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
